4a7f77eda36078daaa6cf21d1fb80b4241bc6302
tushar8133 <tushar8133@gmail.com>
2019-11-06 20:28:50 +0530

    1 file modified

--- timer.ts
+++ timer.ts
@@ -3,28 +3,15 @@ export class Timer {
   contextObj;
   
   constructor(context){
-    this.contextObj = (context)
-    try{
-      clearInterval(window['interval']);
-    }catch(e){}
-
-    // window['interval'] = setInterval( () => {
-    //   this.main();
-    // }, 1000 );
-
-    // requestAnimationFrame( () => {
-    //   console.log(new Date())
-    //   this.main();
-    // });
+    this.contextObj = context;
     this.main();
-
   }
 
   main(){
-    console.log(new Date());
     this.contextObj.resetContext();
     this.contextObj.updateData();
     this.contextObj.draw();
+
     requestAnimationFrame( () => {
       this.main();
     });
